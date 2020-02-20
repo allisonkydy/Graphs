6492164ff19d04b06bee4994d260ef89c518c961
allisonkydy <allisnelly@gmail.com>
2020-02-20 16:54:53 -0600

    adventure - cleaned up traverse code

--- projects/adventure/traverse.py
+++ projects/adventure/traverse.py
@@ -2,27 +2,26 @@
 class Queue():
     def __init__(self):
         self.queue = []
+
     def enqueue(self, value):
         self.queue.append(value)
+
     def dequeue(self):
         if self.size() > 0:
             return self.queue.pop(0)
         else:
             return None
+
     def size(self):
         return len(self.queue)
 
-def is_unexplored(graph):
-    for room in graph:
-        if '?' in graph[room].values():
-            return True
-    return False
 
 def find_unexplored(room):
     for direction in room:
         if room[direction] == '?':
             return direction
     return None
+
 
 def find_direction(current_room, target_room_id):
     for direction in current_room:
@@ -35,30 +34,27 @@ def traverse_map(player):
     path = []
     starting_room = player.current_room.id
     graph = {}
+    unexplored_paths = 0
 
     graph[starting_room] = {}
     for e in player.current_room.get_exits():
         graph[starting_room][e] = '?'
+        unexplored_paths += 1
 
-    # print(graph)
-    reverse_directions = { 'n': 's', 's': 'n', 'e': 'w', 'w': 'e' }
+    reverse_directions = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
 
     # while there are unexplored paths in graph
-    while is_unexplored(graph):
-        # print("graph", graph)
+    while unexplored_paths > 0:
         current_room = player.current_room.id
-        # print("current room", current_room)
 
         # check if current room has unexplored paths
         if '?' in graph[current_room].values():
-        # if so,
+            # if so,
             # find an unexplored direction from current room
             direction = find_unexplored(graph[current_room])
-            # print("direction", direction)
             # travel in that direction
             player.travel(direction)
             next_room = player.current_room.id
-            # print("next room", next_room)
             # add direction to path
             path.append(direction)
             # add directions to graph (replace '?'s)
@@ -67,10 +63,12 @@ def traverse_map(player):
                 graph[next_room] = {}
                 for e in player.current_room.get_exits():
                     graph[next_room][e] = '?'
+                    unexplored_paths += 1
             graph[next_room][reverse_directions[direction]] = current_room
+            unexplored_paths -= 2
 
         else:
-        # if no paths left,
+            # if no paths left,
             # perform a bfs to find nearest room with unexplored path ('?')
 
             # create empty queue
@@ -91,7 +89,7 @@ def traverse_map(player):
 
                 # check if room has any unexplored exits
                 if direction is not None:
-                # if so,
+                    # if so,
                     # convert room ids in path to usable direction
                     for i in range(len(p) - 1):
                         d = find_direction(graph[p[i]], p[i + 1])
@@ -102,9 +100,9 @@ def traverse_map(player):
                     # break loop
                     break
 
-                # check if room has been visisted
+                # check if room has been visited
                 if room not in visited:
-                # if not,
+                    # if not,
                     # mark as visited
                     visited.add(room)
                     # enqueue paths to neighboring rooms
@@ -112,5 +110,5 @@ def traverse_map(player):
                         p_copy = p.copy()
                         p_copy.append(graph[room][e])
                         q.enqueue(p_copy)
-    
+
     return path
