76bfb2660647c94940e441b753791c8ce8ed4891
allisonkydy <allisnelly@gmail.com>
2020-02-20 16:17:43 -0600

    adventure - traversal passing tests for all small maps

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -11,10 +11,10 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-map_file = "maps/test_line.txt"
+# map_file = "maps/test_line.txt"
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
-# map_file = "maps/test_loop_fork.txt"
+map_file = "maps/test_loop_fork.txt"
 # map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
@@ -33,19 +33,19 @@ print(traversal_path)
 
 
 # TRAVERSAL TEST
-# visited_rooms = set()
-# player.current_room = world.starting_room
-# visited_rooms.add(player.current_room)
+visited_rooms = set()
+player.current_room = world.starting_room
+visited_rooms.add(player.current_room)
 
-# for move in traversal_path:
-#     player.travel(move)
-#     visited_rooms.add(player.current_room)
+for move in traversal_path:
+    player.travel(move)
+    visited_rooms.add(player.current_room)
 
-# if len(visited_rooms) == len(room_graph):
-#     print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
-# else:
-#     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
-#     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
+if len(visited_rooms) == len(room_graph):
+    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+else:
+    print("TESTS FAILED: INCOMPLETE TRAVERSAL")
+    print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
 
 
--- projects/adventure/traverse.py
+++ projects/adventure/traverse.py
@@ -1,27 +1,16 @@
 
-
-# def traverse_map(graph):
-#     room_graph = {}
-#     for i in graph:
-#         room_graph[i] = graph[i][1]
-
-#     path = []
-
-#     # create a room_graph (destroyable)
-#     # create empty list for the path
-#     # create empty visited dict
-#     # create empty queue
-
-#     # start at room 0
-#     # add room 0 to queue, along with its directions
-
-#     # while length of visited is less than length of original graph
-#         # dequeue room
-#         # grab directions available from room
-#         # check if any directions available to move
-#             # if so, 
-
-#     return path
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
 
 def is_unexplored(graph):
     for room in graph:
@@ -33,6 +22,13 @@ def find_unexplored(room):
     for direction in room:
         if room[direction] == '?':
             return direction
+    return None
+
+def find_direction(current_room, target_room_id):
+    for direction in current_room:
+        if current_room[direction] == target_room_id:
+            return direction
+    return None
 
 
 def traverse_map(player):
@@ -49,19 +45,20 @@ def traverse_map(player):
 
     # while there are unexplored paths in graph
     while is_unexplored(graph):
-        print("graph", graph)
+        # print("graph", graph)
         current_room = player.current_room.id
-        print("current room", current_room)
+        # print("current room", current_room)
+
         # check if current room has unexplored paths
         if '?' in graph[current_room].values():
         # if so,
             # find an unexplored direction from current room
             direction = find_unexplored(graph[current_room])
-            print("direction", direction)
+            # print("direction", direction)
             # travel in that direction
             player.travel(direction)
             next_room = player.current_room.id
-            print("next room", next_room)
+            # print("next room", next_room)
             # add direction to path
             path.append(direction)
             # add directions to graph (replace '?'s)
@@ -71,23 +68,46 @@ def traverse_map(player):
                 for e in player.current_room.get_exits():
                     graph[next_room][e] = '?'
             graph[next_room][reverse_directions[direction]] = current_room
+
+        else:
         # if no paths left,
             # perform a bfs to find nearest room with unexplored path ('?')
 
             # create empty queue
-            # enqueue path to starting room
+            q = Queue()
+            # enqueue path to current room
+            q.enqueue([current_room])
             # create empty visited set
+            visited = set()
 
             # while the queue is not empty...
+            while q.size() > 0:
                 # dequeue path
+                p = q.dequeue()
                 # grab last room from path
+                room = p[-1]
+
+                direction = find_unexplored(graph[room])
+
                 # check if room has any unexplored exits
+                if direction is not None:
                 # if so,
-                    # convert path to unexplored room into list of directions
-                    # add directions to traversal path
-                    # move player to room using directions
+                    # convert room ids in path to usable direction
+                    for i in range(len(p) - 1):
+                        d = find_direction(graph[p[i]], p[i + 1])
+                        # add direction to traversal path
+                        path.append(d)
+                        # move player to room using direction
+                        player.travel(d)
                     # break loop
+                    break
+
+                else:
                 # if not,
                     # enqueue paths to neighboring rooms
+                    for e in graph[room]:
+                        p_copy = p.copy()
+                        p_copy.append(graph[room][e])
+                        q.enqueue(p_copy)
     
     return path
